b192616c87bd88856eae404bad045d7487704c37
Abhishta Gatya <abhishtagatya@yahoo.com>
2018-05-31 22:25:05 +0700

    Second Initialization : Zomato Carousel Tweaking #7 Temp #3

--- app/routes.py
+++ app/routes.py
@@ -130,15 +130,11 @@ def handle_postback(event):
 
                     counter = 0
                     for restaurant in restaurant_list:
-                        if (restaurant['restaurant']['featured_image'] == '' or restaurant['restaurant']['featured_image'] == None):
-                            thumbnail_image = 'https://b.zmtcdn.com/data/pictures/chains/8/7400098/2391859863f4831c660fcf957fff8742.jpg?output-format=png'
-                        else :
-                            thumbnail_image = (restaurant['restaurant']['featured_image']).replace('webp', 'png')
 
                         restaurant_column = CarouselColumn(
                             text=restaurant['restaurant']['location']['address'],
                             title=restaurant['restaurant']['name'],
-                            thumbnail_image_url=thumbnail_image,
+                            thumbnail_image_url= url_for('static', filename='restaurant/default.png'),
                             actions=[
                             URITemplateAction(
                                 label='Cek Menu', uri=restaurant['restaurant']['menu_url']),
