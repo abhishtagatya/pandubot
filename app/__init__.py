698e1b605a0d1db3af5b9185b2f739c4512121fb
Abhishta Gatya <abhishtagatya@yahoo.com>
2018-05-31 22:08:45 +0700

    Second Initialization : Zomato Carousel Tweaking #7 Temp #1

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from instance.config import LINE_CHANNEL_ACCESS_TOKEN, LINE_CHANNEL_SECRET_TOKEN
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, static)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/LINEUser.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
