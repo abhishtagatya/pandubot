da4fe9b866fe5dcb34217158684846983711279f
Abhishta Gatya <abhishtagatya@yahoo.com>
2018-07-13 01:06:52 +0700

    Point Exchange Carousel #3

--- app/routes.py
+++ app/routes.py
@@ -402,13 +402,13 @@ def handle_postback(event):
                 promotion_column = CarouselColumn(
                     title=str(promotion.promotion_name),
                     text=str(promotion.promotion_description),
-                    action=
+                    actions=[
                     PostbackTemplateAction(
                         label='Tukar Point',
                         data="point_exchange_confirm={promotion_id}".format(
                             promotion_id=promotion.promotion_id
                         ))
-                    )
+                    ])
 
                 promotion_carousel.append(promotion_column)
 
